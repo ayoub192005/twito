5801c22599529029bdf5b5f3efeff7c18844c581
ayoub192005 <89523853+ayoub192005@users.noreply.github.com>
2021-08-25 14:20:30 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+var twit = require('twit');
+var config = require('./config.js');
+
+module.exports = {
+    consumer_key: 'xC1R5DhvItuBXRvqgXltFShp7',
+    consumer_secret: '5B21i28wxUHVd9npptanICy1Kay7S4578t2V3imm7BnV2PkjbZ',
+    access_token: '1670589211-n8kjCx4w0b5W2a3COSOBL3Qb8Epg6G3fhiDMGGj',
+    access_token_secret: '9RFuKDhh2d4GVcVaSXDApFRgntUGJSxP5v7LpC3FmaIhO'
+}
